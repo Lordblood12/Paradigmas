e8519cbf2b49bb463a52b31dce868be3184c237d
Miguel Castillo <mcastillo87@uabc.edu.mx>
2025-09-26 10:26:29 -0700

    Portafolio actualizado

---
+++ Practica1/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    printf("Hello,world");
+
+
+    // TODO: Menu; agregar libro, mostrar libros, agregar miembros
+    // TODO: funciones para cada operacion
+    // memoria dinamica, estructuras, manejo de memoria, nombres
+
+    return 0;
+}
